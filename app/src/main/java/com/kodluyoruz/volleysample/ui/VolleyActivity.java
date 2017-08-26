734d1de107498c7def91c9ced876cec09d984b0c
emrisb <emre.isbarali@gmail.com>
2017-08-26 12:09:01 +0300

    first commit with no traffic :(

---
+++ app/src/main/java/com/kodluyoruz/volleysample/ui/VolleyActivity.java
@@ -0,0 +1,17 @@
+package com.kodluyoruz.volleysample.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kodluyoruz.volleysample.R;
+
+public class VolleyActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volley);
+
+    }
+}
