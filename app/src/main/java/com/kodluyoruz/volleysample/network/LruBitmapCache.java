c5132fb543bd5ee84c72ad82a9ddd96283cd6bcf
emrisb <emre.isbarali@gmail.com>
2017-08-27 10:23:41 +0300

    some changes

---
+++ app/src/main/java/com/kodluyoruz/volleysample/network/LruBitmapCache.java
@@ -0,0 +1,42 @@
+package com.kodluyoruz.volleysample.network;
+
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.toolbox.ImageLoader;
+
+/**
+ * Created by emre on 8/26/2017.
+ */
+
+class LruBitmapCache extends LruCache<String, Bitmap> implements ImageLoader.ImageCache {
+
+    LruBitmapCache() {
+        this(getDefaultLruCacheSize());
+    }
+
+    private LruBitmapCache(int sizeInKiloBytes) {
+        super(sizeInKiloBytes);
+    }
+
+    private static int getDefaultLruCacheSize() {
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+
+        return maxMemory / 8;
+    }
+
+    @Override
+    protected int sizeOf(String key, Bitmap value) {
+        return value.getRowBytes() * value.getHeight() / 1024;
+    }
+
+    @Override
+    public Bitmap getBitmap(String url) {
+        return get(url);
+    }
+
+    @Override
+    public void putBitmap(String url, Bitmap bitmap) {
+        put(url, bitmap);
+    }
+}
--- app/src/main/java/com/kodluyoruz/volleysample/ui/VolleyActivity.java
+++ app/src/main/java/com/kodluyoruz/volleysample/ui/VolleyActivity.java
@@ -2,16 +2,45 @@ package com.kodluyoruz.volleysample.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.kodluyoruz.volleysample.R;
+import com.kodluyoruz.volleysample.network.AppController;
 
-public class VolleyActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+public class VolleyActivity extends AppCompatActivity implements Response.ErrorListener, Response.Listener<JSONObject> {
+
+    private String apiUrl = "https://api.myjson.com/bins/ecemt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volley);
 
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, apiUrl, null, this, this);
+//        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest();
+//        StringRequest stringRequest = new StringRequest();
+        AppController.getInstance().addToRequestQueue(jsonObjectRequest, "Request1");
+
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        try {
+            Toast.makeText(this, response.getJSONObject("GlossDiv").getString("title"), Toast.LENGTH_SHORT).show();
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+        }
     }
 }
